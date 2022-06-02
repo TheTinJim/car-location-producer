4b678c3e7205d43e4793f50181f4054ecb4d67b4
Brandon Northrop <brannorth0414@yahoo.com>
2022-06-01 18:05:30 -0600

    Initial commit

---
+++ src/main/java/com/quintrix/jfs/carlocationproducer/CarLocationProducerApplication.java
@@ -0,0 +1,13 @@
+package com.quintrix.jfs.carlocationproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarLocationProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarLocationProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quintrix/jfs/carlocationproducer/CarLocationProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quintrix.jfs.carlocationproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarLocationProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
